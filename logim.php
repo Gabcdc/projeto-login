e98e49491c7002764fc6ac1d2e3200eb8134817e
gabriel234239094 <130863137+Gabcdc@users.noreply.github.com>
2024-01-09 14:41:30 -0300

    gdd

--- logim.php
+++ logim.php
@@ -1,1 +1,17 @@
-apenda php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-vindo</title>
+</head>
+<body>
+    <h1>Bem-vindo ao meu novo site oficial</h1>
+
+    <?php 
+    $email = $_POST["login"];
+    $senha = $_POST["senha"];
+    
+    ?>
+</body>
+</html>
